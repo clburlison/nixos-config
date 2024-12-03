ae8ebe907b5c869aee48da578751516320d3bd5d
Clayton Burlison <git@clburlison.com>
2024-12-03 16:22:49 -0600

    fix: Correct zsh config

--- users/clburlison/home-manager.nix
+++ users/clburlison/home-manager.nix
@@ -142,34 +142,37 @@ in {
         "kubectl"
         "z"
       ];
+      extraConfig = ''
+        zstyle ':omz:plugins:nvm' lazy yes
+        zstyle ':omz:update' mode disabled
+
+        # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+        # Load the shell dotfiles, and then some:
+        # * ~/.path can be used to extend `$PATH`.
+        # * ~/.extra can be used for other settings you don’t want to commit.
+        for file in ~/.{path,aliases,functions,extra}; do
+            [ -r "$file" ] && [ -f "$file" ] && source "$file";
+        done;
+        unset file;
+
+        # bun completions
+        [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+        # bun
+        export BUN_INSTALL="$HOME/.bun"
+        export PATH="$BUN_INSTALL/bin:$PATH"
+      '';
     };
     # initExtra = builtins.readFile ./dotfiles/zshrc;
     initExtraFirst = ''
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "\${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\${(%):-%n}.zsh" ]]; then
-        source "\${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\${(%):-%n}.zsh"
+      if [[ -r "''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
-    '';
-    extraConfig = ''
-      zstyle ':omz:plugins:nvm' lazy yes
-      zstyle ':omz:update' mode disabled
-
-      # Load the shell dotfiles, and then some:
-      # * ~/.path can be used to extend `$PATH`.
-      # * ~/.extra can be used for other settings you don’t want to commit.
-      for file in ~/.{path,aliases,functions,extra}; do
-          [ -r "$file" ] && [ -f "$file" ] && source "$file";
-      done;
-      unset file;
-
-      # bun completions
-      [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
-
-      # bun
-      export BUN_INSTALL="$HOME/.bun"
-      export PATH="$BUN_INSTALL/bin:$PATH"
     '';
   };
 

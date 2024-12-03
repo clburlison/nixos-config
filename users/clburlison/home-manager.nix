33d26702a6ac42b4cd8d0fbfe45774fd46ddc99c
Clayton Burlison <git@clburlison.com>
2024-12-03 16:29:05 -0600

    fix: Add zsh promptInit for powerlevel10k

--- users/clburlison/home-manager.nix
+++ users/clburlison/home-manager.nix
@@ -134,6 +134,7 @@ in {
     enable = true;
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
+    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     oh-my-zsh = {
       enable = true;
       theme = "powerlevel10k/powerlevel10k"; # "agnoster"; # old theme

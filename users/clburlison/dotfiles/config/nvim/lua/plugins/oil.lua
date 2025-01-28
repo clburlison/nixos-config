979be72fee51b35c1e59d02cdf3b1bb61f39e3ec
Clayton Burlison <git@clburlison.com>
2025-01-28 11:27:20 -0600

    fix: Oil options

--- users/clburlison/dotfiles/config/nvim/lua/plugins/oil.lua
+++ users/clburlison/dotfiles/config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,19 @@ return {
   'stevearc/oil.nvim',
   opts = {
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        local hidden_files = {
+          ['.DS_Store'] = true,
+          ['Thumbs.db'] = true,
+          ['.git'] = true,
+        }
+        return hidden_files[name] == true
+      end,
+    },
   },
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
 }

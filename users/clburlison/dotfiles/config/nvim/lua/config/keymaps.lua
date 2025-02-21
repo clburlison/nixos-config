187fa827d5c8c12bd99ba5621e42d581310d2afd
Clayton Burlison <git@clburlison.com>
2025-02-21 12:40:17 -0600

    feat: add more neovim keyboard shortcuts
    many of these are pulled from LazyVim as they are a pretty sensible
    default.

--- users/clburlison/dotfiles/config/nvim/lua/config/keymaps.lua
+++ users/clburlison/dotfiles/config/nvim/lua/config/keymaps.lua
@@ -21,20 +21,49 @@ set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]ui
 -- or just use <C-\><C-n> to exit terminal mode
 set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
-set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Move to window using the <ctrl> hjkl keys
+set('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
+set('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
+set('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
+set('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
+-- Resize window using <ctrl> arrow keys
+set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+
+-- Create window splits
+set('n', '<leader>ww', '<C-W>v', { desc = 'Split Window Right', remap = true })
+set('n', '<leader>wW', '<C-W>s', { desc = 'Split Window Below', remap = true })
+set('n', '<leader>wd', '<C-W>c', { desc = 'Delete Window', remap = true })
+
+-- tabs
+set('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
+set('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
+set('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
+set('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New Tab' })
+set('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
+set('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
+set('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
+
+-- Move Lines
+set('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
+set('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
+set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+set('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
+set('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
+
+-- new file
+set('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
+
+-- quit
+set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+
 --
---  See `:help wincmd` for a list of all window commands
-set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Functions below here
+--
 
 -- Toggle true/false
 vim.fn.setreg('t', '^f:wye ciw' .. string.char(18) .. '=@"=~"true"?"false":"true"' .. string.char(27) .. string.char(27))
@@ -45,7 +74,7 @@ set('v', '<leader>tq', ':s/^\\(\\s*\\)\\(.*\\)$/\\1"\\2"/g<CR>', { desc = 'Add d
 
 -- Function to toggle diagnostic virtual text
 local function toggle_virtual_text()
-  local current_config = vim.diagnostic.config()
+  local current_config = vim.diagnostic.config() or {}
   local virtual_text_enabled = current_config.virtual_text or false
   -- Toggle the state
   virtual_text_enabled = not virtual_text_enabled

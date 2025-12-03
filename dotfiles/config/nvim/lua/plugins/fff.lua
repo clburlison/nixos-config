d3b0d4e9ff2645dea32ae2f6414f3e06dcf2b750
Clayton Burlison <git@clburlison.com>
2025-12-03 12:16:49 -0600

    chore(nvim): Update external deps

--- dotfiles/config/nvim/lua/plugins/fff.lua
+++ dotfiles/config/nvim/lua/plugins/fff.lua
@@ -1,7 +1,12 @@
 return {
   'dmtrKovalenko/fff.nvim',
   enabled = true,
-  build = 'cargo build --release',
+  -- build = 'cargo build --release',
+  build = function()
+    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+    require('fff.download').download_or_build_binary()
+  end,
   opts = {},
   keys = {
     {

857f98643a644accc8092f8dba59dd4ecbbb2997
Clayton Burlison <git@clburlison.com>
2025-12-21 14:03:28 -0600

    feat(zellij): New nvim/zellij navigator plugin

--- dotfiles/config/nvim/lua/plugins/zellij-nav.lua
+++ dotfiles/config/nvim/lua/plugins/zellij-nav.lua
@@ -1,6 +1,33 @@
+-- https://github.com/hiasr/vim-zellij-navigator
 return {
   {
+    'mrjones2014/smart-splits.nvim',
+    enabled = true,
+    lazy = false,
+    event = 'VeryLazy',
+    keys = {
+      -- moving between splits
+      { '<C-h>', '<cmd>SmartCursorMoveLeft<cr>', { silent = true, desc = 'move cursor left' } },
+      { '<C-j>', '<cmd>SmartCursorMoveDown<cr>', { silent = true, desc = 'move cursor down' } },
+      { '<C-k>', '<cmd>SmartCursorMoveUp<cr>', { silent = true, desc = 'move cursor up' } },
+      { '<C-l>', '<cmd>SmartCursorMoveRight<cr>', { silent = true, desc = 'move cursor right' } },
+      -- resizing splits
+      -- these keymaps will also accept a range,
+      -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+      { '<A-h>', '<cmd>SmartResizeLeft<cr>', { silent = true, desc = 'resize left' } },
+      { '<A-j>', '<cmd>SmartResizeDown<cr>', { silent = true, desc = 'resize down' } },
+      { '<A-k>', '<cmd>SmartResizeUp<cr>', { silent = true, desc = 'resize up' } },
+      { '<A-l>', '<cmd>SmartResizeRight<cr>', { silent = true, desc = 'resize right' } },
+      -- swapping buffers between windows
+      { '<leader><leader>h', '<cmd>SmartSwapLeft<cr>', { silent = true, desc = 'swap buffer left' } },
+      { '<leader><leader>j', '<cmd>SmartSwapDown<cr>', { silent = true, desc = 'swap buffer down' } },
+      { '<leader><leader>k', '<cmd>SmartSwapUp<cr>', { silent = true, desc = 'swap buffer up' } },
+      { '<leader><leader>l', '<cmd>SmartSwapRight<cr>', { silent = true, desc = 'swap buffer right' } },
+    },
+  },
+  {
     'swaits/zellij-nav.nvim',
+    enabled = false,
     lazy = true,
     event = 'VeryLazy',
     keys = {
@@ -10,15 +37,5 @@ return {
       { '<c-l>', '<cmd>ZellijNavigateRightTab<cr>', { silent = true, desc = 'navigate right or tab' } },
     },
     opts = {},
-  },
-  {
-    -- Not sure why this plugin was mentioned as required? Navigation seems to work
-    -- just fine without it. With it enabled it drastically slows down neovim's shutdown.
-    'hiasr/vim-zellij-navigator.nvim',
-    enabled = false,
-    event = 'VeryLazy',
-    config = function()
-      require('vim-zellij-navigator').setup()
-    end,
   },
 }

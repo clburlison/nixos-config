9f83ac4d8b3d5fb9f44ad856cbd0f5710b111f47
Clayton Burlison <git@clburlison.com>
2025-12-26 21:37:41 -0600

    chore(nvim): Bump external deps + comments

--- dotfiles/config/nvim/lua/plugins/telescope.lua
+++ dotfiles/config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  version = '0.2.0',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
